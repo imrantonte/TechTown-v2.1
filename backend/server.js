6c8ee2fec10d0d323812f80b3bbe75a94875c4bf
imran hasmi <imrantonte@users.noreply.github.com>
2026-07-19 20:14:23 +0600

    chore: make CORS robust to trailing slashes

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,9 @@ connectDB();
 app.use(helmet()); 
 
 // 2. Cross-Origin Resource Sharing
+const frontendUrl = process.env.FRONTEND_URL;
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: frontendUrl ? [frontendUrl, frontendUrl.replace(/\/$/, '')] : 'http://localhost:5173',
     credentials: true 
 }));
 
